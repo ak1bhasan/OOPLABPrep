e52436cea4a19e73b7ef05d3b1fceb8e4b9a9270
Md. Masudul Hasan Akib <136964348+ak1bhasan@users.noreply.github.com>
2025-08-12 01:50:52 +0600

    Add files via upload

--- MUDgitialWallet/src/main/java/com/mycompany/mudgitialwallet/MUDgitialWallet.java
+++ MUDgitialWallet/src/main/java/com/mycompany/mudgitialwallet/MUDgitialWallet.java
@@ -7,14 +7,20 @@ public class MUDgitialWallet {
         dg.addMoney(500);
         dg.sendMoney(2002, 500.50);
         dg.withdraw(500.55);
+
+        try {
+            DigitalWallet dg1 = new DigitalWallet("Jax", 2002, 500.00);
+            dg1.addMoney(500);
+            dg1.sendMoney(3003, 1000.5);
+            dg1.withdraw(500.55);
+        } catch (InvalidAmountException | InsufficientBalanceException e) {
+            System.out.println("Transaction failed: " + e.getMessage());
+        }
         
-        try{
-        DigitalWallet dg1 = new DigitalWallet("Jax", 2002, 500.00);
-        dg1.addMoney(500);
-        dg1.sendMoney(3003, 1000.5);
-        dg1.withdraw(500.55);
-        }catch(InvalidAmountException | InsufficientBalanceException e) {
-            System.out.println("Transaction failed: " +  e.getMessage());
+        try {
+            dg.addMoney(-500);
+        } catch(InvalidAmountException e) {
+            System.out.println("Transaction failed: " + e.getMessage() );
         }
         
     }
